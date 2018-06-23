6f61556205397763f7255f9cb1e0af88aa7fba78
kazaf0322 <yeelamz@gmail.com>
2018-06-23 17:42:26 +0800

    Update gen_shellcode.py

--- gen_shellcode.py
+++ gen_shellcode.py
@@ -1,6 +1,6 @@
 from subprocess import check_output
 
-loader_offset = int(check_output('nm -g payload/payload.dylib | grep "T _load"',
+loader_offset = int(check_output('nm -g payload.dylib | grep "T _load"',
         shell=True).split()[0], 16)
 inp = open('shellcode.in.s').read()
 outp = inp.replace('OFFSET_LOAD', hex(loader_offset))
